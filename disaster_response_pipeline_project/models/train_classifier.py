4eeb859c3aa80b7c70b54d52cc5ba872973dc91d
samusam91 <milazzosamuel@gmail.com>
2024-02-20 11:32:32 +0100

    new main

--- disaster_response_pipeline_project/models/train_classifier.py
+++ disaster_response_pipeline_project/models/train_classifier.py
@@ -1,52 +1,102 @@
-import sys
+import numpy as np
+import pandas as pd
+from sqlalchemy import create_engine
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import classification_report
+from sklearn.model_selection import GridSearchCV
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+import re
+
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
+
+db_path = 'sqlite:///DisasterResponse.db'
+table = 'samtable'
 
 
 def load_data(database_filepath):
-    pass
+    # Create an SQLite engine
+    engine = create_engine(database_filepath)
+
+    # Load dataset from database with read_sql_table
+    df = pd.read_sql_table(table, engine)
+    X = df['message']
+    Y = df.drop(['id', 'message', 'original', 'genre'], axis=1)
+    category_names = Y.columns.tolist()
+    return X, Y, category_names
 
 
 def tokenize(text):
-    pass
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+    stop_words = stopwords.words("english")
+
+    clean_tokens = []
+    for token in tokens:
+        if token not in stop_words:
+            if re.match('^[a-zA-Z0-9_-]*$', token):  # Check the underscore and other symbols
+                clean_tokens.append(lemmatizer.lemmatize(token).lower().strip())
+
+    return clean_tokens
 
 
 def build_model():
-    pass
+    # Create a pipeline
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultiOutputClassifier(RandomForestClassifier()))
+    ])
+
+    return pipeline
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    # Predictions
+    Y_pred = model.predict(X_test)
+
+    # Classification report
+    print("Classification Report:")
+    for i, col in enumerate(category_names):
+        print(col)
+        print(classification_report(Y_test[col], Y_pred[:, i]))
 
 
 def save_model(model, model_filepath):
-    pass
+    # Save the model to a file
+    import joblib
+    joblib.dump(model, model_filepath)
 
 
 def main():
-    if len(sys.argv) == 3:
-        database_filepath, model_filepath = sys.argv[1:]
-        print('Loading data...\n    DATABASE: {}'.format(database_filepath))
-        X, Y, category_names = load_data(database_filepath)
-        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
-        
-        print('Building model...')
-        model = build_model()
-        
-        print('Training model...')
-        model.fit(X_train, Y_train)
-        
-        print('Evaluating model...')
-        evaluate_model(model, X_test, Y_test, category_names)
+    database_filepath = db_path
+    model_filepath = "classifier.pkl"
 
-        print('Saving model...\n    MODEL: {}'.format(model_filepath))
-        save_model(model, model_filepath)
+    print('Loading data...\n    DATABASE: {}'.format(database_filepath))
+    X, Y, category_names = load_data(database_filepath)
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+    
+    print('Building model...')
+    model = build_model()
+    
+    print('Training model...')
+    model.fit(X_train, Y_train)
+    
+    print('Evaluating model...')
+    evaluate_model(model, X_test, Y_test, category_names)
 
-        print('Trained model saved!')
+    print('Saving model...\n    MODEL: {}'.format(model_filepath))
+    save_model(model, model_filepath)
 
-    else:
-        print('Please provide the filepath of the disaster messages database '\
-              'as the first argument and the filepath of the pickle file to '\
-              'save the model to as the second argument. \n\nExample: python '\
-              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
+    print('Trained model saved!')
 
 
 if __name__ == '__main__':
